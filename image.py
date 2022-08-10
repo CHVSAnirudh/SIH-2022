1248e2d962bf6ae950c5f3ab74fd708e11a02020
sripriya.maturi <sripriya.maturi@cropin.com>
2022-08-10 15:26:40 +0530

    image generation for dataset

---
+++ image.py
@@ -0,0 +1,45 @@
+import random
+from PIL import Image
+import os
+
+no_of_fishes = random.randint(6,16)
+
+#print(no_of_fishes)
+whitebg_ht = 2830
+whitebg_width = 4244
+
+min_height = whitebg_ht // 10
+max_height = whitebg_ht // 5
+
+min_width = whitebg_width // 10
+max_width = whitebg_width // 5
+
+images = []
+for i in range(no_of_fishes):
+    im = random.choice(os.listdir(r"C:\Users\mahes\Desktop\sih\dataset"))
+    #path1 = os.path.normpath("my_path/this_way")
+    im = r"C:\Users\mahes\Desktop\sih\dataset/"+im
+    # print(im)
+    # im="1.png"
+    img = Image.open(im)
+    images.append(img)
+#print(images)
+
+#resizing 
+for i in range(no_of_fishes):
+    new_height = random.randint(min_height, max_height+1)
+    new_width = random.randint(min_width, max_width+1)
+    #print(images[i].size)
+    images[i] = images[i].resize((new_width, new_height))
+    #print(images[i].size)
+
+#rotating
+angle = random.randint(0,361)
+for i in range(no_of_fishes):
+    images[i] = images[i].rotate(angle)
+
+
+white_img = Image.open("whitebg.jpg")
+white_img.paste(images[0],(4244//2,2830//2))
+
+white_img.save("5.png")
