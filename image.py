0d45bea32e8d8cb22a0d5d04eaf7c39a50fbe2f5
anirudh chebolu <ani1005sai@gmail.com>
2022-08-10 17:45:20 +0530

    black bg bug fix

--- image.py
+++ image.py
@@ -1,12 +1,38 @@
 import random
 from PIL import Image
 import os
-
+import numpy as np
+import cv2
 no_of_fishes = random.randint(6,16)
 
+
+def remove_black_bg(image_name):
+    # Convert image to image gray
+    image = cv2.imread(image_name, 1)
+    
+    tmp = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    
+    # Applying thresholding technique
+    _, alpha = cv2.threshold(tmp, 0, 255, cv2.THRESH_BINARY)
+    
+    # Using cv2.split() to split channels 
+    # of coloured image
+    b, g, r = cv2.split(image)
+    
+    # Making list of Red, Green, Blue
+    # Channels and alpha
+    rgba = [b, g, r, alpha]
+    
+    # Using cv2.merge() to merge rgba
+    # into a coloured/multi-channeled image
+    dst = cv2.merge(rgba, 4)
+    
+    # Writing and saving to a new image
+    cv2.imwrite(image_name, dst)
+
 #print(no_of_fishes)
-whitebg_ht = 2830
-whitebg_width = 4244
+whitebg_ht = 612
+whitebg_width = 408
 
 min_height = whitebg_ht // 5
 max_height = whitebg_ht // 4
@@ -16,8 +42,10 @@ max_width = whitebg_width // 4
 
 images = []
 for i in range(no_of_fishes):
-    im = random.choice(os.listdir(r"C:\Users\mahes\Desktop\sih\SIH-2022\dataset"))
-    im = r"C:\Users\mahes\Desktop\sih\SIH-2022\dataset/"+im
+    im = random.choice(os.listdir(r"dataset"))
+    print(im)
+    im = r"dataset/"+im
+    #remove_black_bg(im)
     img = Image.open(im)
     images.append(img)
 
@@ -36,7 +64,7 @@ for i in range(no_of_fishes):
     images[i] = images[i].rotate(angle)
 
 
-white_img = Image.open("whitebg.jpg")
+white_img = Image.open("whitebg.png")
 
 #selecting area randomly to paste fishes
 for i in range(no_of_fishes):
