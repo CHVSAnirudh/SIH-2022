4e7dfa1985039c5b5c5ca803425efa2260d81264
sripriya.maturi <sripriya.maturi@cropin.com>
2022-08-15 02:53:51 +0530

    removed overlapped labels

--- image.py
+++ image.py
@@ -6,7 +6,7 @@ import numpy as np
 
 no_of_fishes = random.randint(15,26)
 f = open("1.txt", "a")
-#print(no_of_fishes)
+print(no_of_fishes)
 whitebg_ht = 2830
 whitebg_width = 4244
 
@@ -51,14 +51,15 @@ white_img = Image.open("whitebg.png")
 white_img = white_img.convert("RGBA")
 
 #selecting area randomly to paste fishes
+mid_pts = []
 for i in range(no_of_fishes):
-    string = ""
+    type = 0
     if(img_names[i][:5] == "catla"):
-        string += "0 "
+        type = 0
     elif(img_names[i][:4] == "rohu"):
-        string += "1 "
+        type = 1
     else:
-        string += "2 "
+        type = 2
     random_width_min = (whitebg_width//2) - (whitebg_width//4)
     random_width_max = (whitebg_width//2) + 500
     random_height_min = (whitebg_ht//2) - (whitebg_ht//4)
@@ -67,9 +68,24 @@ for i in range(no_of_fishes):
     random_height = random.randint(random_height_min, random_height_max)
     mid_x = (random_width + images[i].size[0])/(2 * whitebg_width)
     mid_y = (random_height + images[i].size[1])/(2* whitebg_ht)
-    string += str(mid_x)+" "+str(mid_y)+" "+str((3*images[i].size[0])/(2*whitebg_width))+" "+str((images[i].size[1]*3)/(whitebg_ht*2))+"\n"
-    f.write(string)
+    new_mid_pts = []
+    radius = images[i].size[1]/(40*whitebg_ht)
+    for t,ty,x,y in mid_pts:
+        if(x-radius < mid_x and x+radius> mid_x):
+            continue
+        if(y-radius < mid_y and y+radius > mid_y):
+            continue
+        new_mid_pts.append((t,ty,x,y))
+    mid_pts = new_mid_pts
+    mid_pts.append((i,type,mid_x,mid_y))
+    # string += str(mid_x)+" "+str(mid_y)+" "+str((3*images[i].size[0])/(2*whitebg_width))+" "+str((images[i].size[1]*3)/(whitebg_ht*2))+"\n"
+    # f.write(string)
     white_img.paste(images[i],(random_width,random_height), images[i])
+
+for i,type, x,y in mid_pts:
+    string = str(type) + " "
+    string += str(x)+" "+str(y)+" "+str((3*images[i].size[0])/(2*whitebg_width))+" "+str((images[i].size[1]*3)/(whitebg_ht*2))+"\n"
+    f.write(string)
 
 white_img.save("5.png")
 f.close()
