7bcd3e6a95fc9af55b173a571f1ecf3d9b128a68
sripriya.maturi <sripriya.maturi@cropin.com>
2022-08-21 21:34:25 +0530

    added register.py

--- registers.py
+++ registers.py
@@ -4,4 +4,19 @@ def fisherman_register(obj):
     client = MongoClient("mongodb+srv://test:test@cluster0.zppnq.mongodb.net/debuggers?retryWrites=true&w=majority")
     db = client.get_database('SIH')
     records = db.user_fisherman
-    records = list(records.find())
+    #records = list(records.find())
+    records.insert_one(obj)
+
+def weighbridge_register(obj):
+    client = MongoClient("mongodb+srv://test:test@cluster0.zppnq.mongodb.net/debuggers?retryWrites=true&w=majority")
+    db = client.get_database('SIH')
+    records = db.user_weighbridge
+    #records = list(records.find())
+    records.insert_one(obj)
+
+def govt_register(obj):
+    client = MongoClient("mongodb+srv://test:test@cluster0.zppnq.mongodb.net/debuggers?retryWrites=true&w=majority")
+    db = client.get_database('SIH')
+    records = db.user_govt
+    #records = list(records.find())
+    records.insert_one(obj)
