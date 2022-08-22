3560d01260d833d1adef1802b4ff6347652b99df
sripriya.maturi <sripriya.maturi@cropin.com>
2022-08-23 00:43:47 +0530

    turtle_detection

--- turtle_detection.py
+++ turtle_detection.py
@@ -6,24 +6,9 @@ import glob
 import json
 import math
 def predict_image(image,weight):
-    # passing the image to model
+    turtles = 0
+    fishes = 0
 
-    # exp = glob.glob('./runs/detect/*/')
-    # print(exp)
-    # exp.remove('./runs/detect/exp/')
-    # exp = sorted(exp,key = lambda x:int(x[17:-1:]),reverse=True)
-    # print(exp[0])
-    # l = glob.glob('{}/crops/*/'.format(exp[0]))
-    # print(l)
-    # img = []
-    # for x in l:
-    #     folders = glob.glob('{}/*jpg'.format(x))
-    #     img.extend(folders)
-    # print(img)
-    catla = 0
-    rohu = 0
-    mori = 0
-    #for x in img:
     os.system("python detect.py --weights specie_detection.pt --source {} --conf 0.01 --save-txt".format(image))  
     exp = glob.glob('./runs/detect/*/')
     print(exp)
@@ -37,40 +22,42 @@ def predict_image(image,weight):
             Lines = f.readlines()
             for line in Lines:
                 if line[0] == '0':
-                    catla+=1
+                    turtles+=1
                 elif line[0] == '1':
-                    mori+=1
-                else:
-                    rohu+=1
-    result = []
-    s = catla+rohu+mori
-    catla_weight = (catla/s)*weight
-    catla_weight = round(catla_weight)
-    rohu_weight = (rohu/s)*weight
-    rohu_weight = round(rohu_weight)
-    mori_weight = (mori/s)*weight
-    mori_weight = round(mori_weight)
-    catla_proportion = (catla/s)*100
-    catla_proportion = round(catla_proportion)
-    rohu_proportion = (rohu/s)*100
-    rohu_proportion = round(rohu_proportion)
-    mori_proportion = (mori/s)*100
-    mori_proportion = round(mori_proportion)
-    if catla_weight>0:
-        result.append({"name":"catla","weight":catla_weight,"proportion":catla_proportion})
-    if rohu_weight>0:
-        result.append({"name":"rohu","weight":rohu_weight,"proportion":rohu_proportion})
-    if mori_weight>0:
-        result.append({"name":"mori","weight":mori_weight,"proportion":mori_proportion})
+                    fishes+=1
+    result = {}
+    result["turtles":turtles]
+    if turtles != 0:
+        catch_dbupdate()
+    return {'status': 'Sucess', 'turtles': turtles}
+    # s = catla+rohu+mori
+    # catla_weight = (catla/s)*weight
+    # catla_weight = round(catla_weight)
+    # rohu_weight = (rohu/s)*weight
+    # rohu_weight = round(rohu_weight)
+    # mori_weight = (mori/s)*weight
+    # mori_weight = round(mori_weight)
+    # catla_proportion = (catla/s)*100
+    # catla_proportion = round(catla_proportion)
+    # rohu_proportion = (rohu/s)*100
+    # rohu_proportion = round(rohu_proportion)
+    # mori_proportion = (mori/s)*100
+    # mori_proportion = round(mori_proportion)
+    # if catla_weight>0:
+    #     result.append({"name":"catla","weight":catla_weight,"proportion":catla_proportion})
+    # if rohu_weight>0:
+    #     result.append({"name":"rohu","weight":rohu_weight,"proportion":rohu_proportion})
+    # if mori_weight>0:
+    #     result.append({"name":"mori","weight":mori_weight,"proportion":mori_proportion})
 
-    if catla==0 and rohu==0 and mori==0:
-        return {'status': 'Sucess', 'result': "The model is still immature and only detects catla rohu and mori, either the image contains fishes of other species or come back to us with better resolution image"} 
+    # if catla==0 and rohu==0 and mori==0:
+    #     return {'status': 'Sucess', 'result': "The model is still immature and only detects catla rohu and mori, either the image contains fishes of other species or come back to us with better resolution image"} 
     return {'status': 'Sucess', 'result': result}
 
 def catch_dbupdate(obj):
     client = MongoClient("mongodb+srv://test:test@cluster0.zppnq.mongodb.net/debuggers?retryWrites=true&w=majority")
     db = client.get_database('SIH')
-    records = db.user_fisherman
+    records = db.catch_endangered
     #records = list(records.find())
     record = records.find_one({"username":obj.get('username')})
     print(record)
