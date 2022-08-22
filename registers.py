3e6f153b851958789c9f44dad945f353d282f8c7
sripriya.maturi <sripriya.maturi@cropin.com>
2022-08-22 16:38:41 +0530

    checking if username already exists

--- registers.py
+++ registers.py
@@ -5,6 +5,9 @@ def fisherman_register(obj):
     db = client.get_database('SIH')
     records = db.user_fisherman
     #records = list(records.find())
+    records = list(records.find_one({'username':obj.get('username')}))
+    if len(records)>0:
+        return {"Message":"Username already taken try a different one", "Status":"402"}
     records.insert_one(obj)
     return {"Message":"fisherman added", "Status":"200"}
 
@@ -13,6 +16,9 @@ def weighbridge_register(obj):
     db = client.get_database('SIH')
     records = db.user_weighbridge
     #records = list(records.find())
+    records = list(records.find_one({'username':obj.get('username')}))
+    if len(records)>0:
+        return {"Message":"Username already taken try a different one", "Status":"402"}
     records.insert_one(obj)
     return {"Message":"Weighbridge added", "Status":"200"}
 
